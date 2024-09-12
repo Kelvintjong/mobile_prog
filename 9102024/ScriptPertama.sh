1904774d7180f7b1ff3b8971715c2ccd96447158
kelvintjong <kelvintjong117@gmail.com>
2024-09-12 11:13:06 +0700

    Script Pertama

---
+++ 9102024/ScriptPertama.sh
@@ -0,0 +1,29 @@
+#echo "siapa nama anda?"
+#command ini untuk berinteraksi dengan terminal
+#setelah "read" merupakan nama variabel yang digunakan untuk menampung jawaban atau interaksi dari user/pengguna terhadap terminal
+#read name
+#untuk memanggil variabel yang kita gunakan untuk menampung jawaban atau interaksi $
+#echo "nama saya adalah $name"
+
+#echo "selamat datang di kalkulator"
+#echo "berapa panjang persegi panjang"
+#read "panjang"
+#echo "berapa lebar persegi panjang?"
+#read lebar
+#keliling=$(( 2 * (panjang + lebar) ))
+
+#echo "keliling persegi panjang dengan panjang $p dan lebar $1 adalah: $keliling"
+
+#echo "masukan jari jari kerucut"
+
+#read r
+
+#echo "masukan tinggi kerucut"
+
+#read t
+
+#phi=3,14
+
+#volume=$((1/3 * phi *(r^2)*t))
+
+#echo "ini hasil dari jari jari $r dan tinggi nya $t adalah $volume"
